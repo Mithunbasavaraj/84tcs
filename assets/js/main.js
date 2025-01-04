0e023be9579f876b3156093d15079c576e71b990
Mithun B <mithunbasavaraj057@gmail.com>
2025-01-04 22:56:56 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -598,3 +598,24 @@
       );
     });
 })(jQuery);
+
+
+
+var swiper = new Swiper(".mentor-slide", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    520: { slidesPerView: 2 },
+    950: { slidesPerView: 3 },
+  },
+});
